5690dc76b99835e59dec7b24f1d7396ce76191e7
Dolapo Abdul <kellyworks91@gmail.com>
2022-10-16 05:28:36 +0000

    Intel Syntax2.3

--- 0x00-hello_world/100-main.s
+++ 0x00-hello_world/100-main.s
@@ -10,67 +10,37 @@
 main:
 .LFB0:
 	.cfi_startproc
-	endbr32
-	lea	ecx, 4[esp]
-	.cfi_def_cfa 1, 0
-	and	esp, -16
-	push	DWORD PTR -4[ecx]
-	push	ebp
-	mov	ebp, esp
-	.cfi_escape 0x10,0x5,0x2,0x75,0
-	push	ebx
-	push	ecx
-	.cfi_escape 0xf,0x3,0x75,0x78,0x6
-	.cfi_escape 0x10,0x3,0x2,0x75,0x7c
-	call	__x86.get_pc_thunk.ax
-	add	eax, OFFSET FLAT:_GLOBAL_OFFSET_TABLE_
-	sub	esp, 12
-	lea	edx, .LC0@GOTOFF[eax]
-	push	edx
-	mov	ebx, eax
-	call	printf@PLT
-	add	esp, 16
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	lea	rdi, .LC0[rip]
 	mov	eax, 0
-	lea	esp, -8[ebp]
-	pop	ecx
-	.cfi_restore 1
-	.cfi_def_cfa 1, 0
-	pop	ebx
-	.cfi_restore 3
-	pop	ebp
-	.cfi_restore 5
-	lea	esp, -4[ecx]
-	.cfi_def_cfa 4, 4
+	call	printf@PLT
+	mov	eax, 0
+	pop	rbp
+	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE0:
 	.size	main, .-main
-	.section	.text.__x86.get_pc_thunk.ax,"axG",@progbits,__x86.get_pc_thunk.ax,comdat
-	.globl	__x86.get_pc_thunk.ax
-	.hidden	__x86.get_pc_thunk.ax
-	.type	__x86.get_pc_thunk.ax, @function
-__x86.get_pc_thunk.ax:
-.LFB1:
-	.cfi_startproc
-	mov	eax, DWORD PTR [esp]
-	ret
-	.cfi_endproc
-.LFE1:
 	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
-	.align 4
+	.align 8
 	.long	 1f - 0f
 	.long	 4f - 1f
 	.long	 5
 0:
 	.string	 "GNU"
 1:
-	.align 4
+	.align 8
 	.long	 0xc0000002
 	.long	 3f - 2f
 2:
 	.long	 0x3
 3:
-	.align 4
+	.align 8
 4:
