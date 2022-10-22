e4842edaa8022de13691753e32f80f8c2007a8ef
Dolapo Abdul <kellyworks91@gmail.com>
2022-10-22 03:41:05 +0000

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers 0 to 9
+ * Return: numbers
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
