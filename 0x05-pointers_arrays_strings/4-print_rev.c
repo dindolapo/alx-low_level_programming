3e86d6c0047cf26fa8bddea2f25d0a507bb6ba38
Dolapo Abdul <kellyworks91@gmail.com>
2022-10-26 01:28:11 +0000

    pointer exercise

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: input string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 63; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
