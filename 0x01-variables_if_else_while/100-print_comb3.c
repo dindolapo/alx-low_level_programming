657bdfbc0dbe82ff9b90752d7327055d803c3fd1
Dolapo Abdul <kellyworks91@gmail.com>
2022-10-28 04:20:04 +0000

    if else while advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	i = 0;
+	while (i < 99)
+	{
+		j = i % 10;
+		k = i / 10;
+
+		if (k < j)
+		{
+			putchar(k + '0');
+			putchar(j + '0');
+
+			if (i < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
