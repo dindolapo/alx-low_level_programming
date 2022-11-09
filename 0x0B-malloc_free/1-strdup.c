48fbfbd797ef5e153100f3175333b4485b95dcf0
Dolapo Abdul <kellyworks91@gmail.com>
2022-11-09 04:37:48 +0000

    task #0 and #1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory containing a
+ * string
+ * @str: argument for input array
+ * Return: NULL if str = NULL and NULL if memory is insufficient
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i, j;
+
+	if (str == NULL)
+		return NULL;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	s = malloc(sizeof(*str) * i);
+
+		if (s == NULL)
+			return NULL;
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		s[j] = str[j];
+	}
+	s[j] = '\0';
+
+	return s;
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+i am dok
+this is the repository for solutions to exercises on malloc and free
