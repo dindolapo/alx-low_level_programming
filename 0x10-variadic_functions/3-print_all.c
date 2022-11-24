f222b664ec4143e6d56258cd501a9a0166e39c99
Dolapo Abdul <kellyworks91@gmail.com>
2022-11-24 09:06:19 +0000

    bug fixed

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -34,7 +34,7 @@ void print_all(const char * const format, ...)
 			case 's':
 				str = va_arg(pa, char *);
 				if (!str)
-					printf("(nil)");
+					str = "(nil)";
 
 				printf("%s%s", seperator, str);
 				break;
