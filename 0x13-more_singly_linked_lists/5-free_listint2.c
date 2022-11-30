4296ff6fc01bb62b243d9a9731e80baabcbb8b78
Dolapo Abdul <kellyworks91@gmail.com>
2022-11-30 11:59:59 +0000

    #5 - free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - free a linked list
+ * @head: head of the linked list
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp = head;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = NULL;
+	}
+}
